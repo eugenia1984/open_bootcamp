a5e616cf690b624ffcc43079472e9c0cfa90f009
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-20 20:13:26 -0300

    sigo con JS y las variables

--- 03_javascript_basico/02_sintaxis_variables_palabras_reservadas.js
+++ 03_javascript_basico/02_sintaxis_variables_palabras_reservadas.js
@@ -38,7 +38,67 @@ console.log(null === undefined)
 
 
 /****** 2.2 Declaracion de variables y escritura dinamica **********/
+// Hay tres formas de definir las variables: var(tienen un scoope global), let(tienen un scoope local, existen en el ambito dentro del cual son declaradas) y const(estas en realidad son constantes, no varian)
+// Las variables pueden primero ser declaradas y luego asignarle el valor, o hacer ambas cosas juntas
+let variable = 1;
+console.log(`variable: ${variable}`); // 1
+variable = 2; // modifico el dato que guarda (su valor)
+console.log(`variable: ${variable}`); // 2
+const hola = "Hola soy una constante";
+// hola = "Chau"; me da error, no puedo redefinir una constante
+// Las constantes deben declararse y asignarse el valor, todo al mismo tiempo
+
+// Ejemplo con VAR
+var variable2 = "Hola soy una variable VAR";
+
+for (var i = 0; i < 3; i++) {
+  var variable2 = "Soy la segunda variable"
+}
+// Mi ciclo for me va a cambiar al definicion de la cariable
+console.log(variable2); //  "Soy la segunda variable"
+
+// Ahora lo comparamos con LET
+let variableLet = "Hola soy una variable LET";
+
+for (var i = 0; i < 3; i++) {
+    let variableLet = "Soy la segunda variable LET";
+}
+// No se me reescribe, ya que la otra existe sol odentro del for, pero no es buena practica nombrar a dos variables iguales, es mejor que dentro del for tenga otro nombre
+console.log(variableLet);  // "Hola soy una variable LET";
+
+// con typeof podemos saber de que tipo son
+let numero3 = 3;
+console.log(typeof numero3); // Number
+let cadena2 = "Euge";
+console.log(typeof cadena2); // String
 
 /****** 2.3 Notacion en JavaSCript **********/
+
+// ; -> Delimitar el final de una línea
+const b = 4;
+b + 4;
+
+// [] -> listas, arreglos o arrays
+const ar = [1, 2, 3, 4]
+console.log(ar[2])
+
+// () -> Funciones
+function suma(a, b) {
+  return a + b;
+}
+
+// {} -> Llaves para objetos, funciones y estructuras de control
+const movil = {
+  anchura: 5,
+  altura: 10
+}
+
+// . -> Se utiliza en los objetos para acceder a los atributos se le dice DOT NOTATION
+// movil.anchura
+console.log(movil.anchura); // 5
+
+if (true) {
+  const constante2 = "hola" // si la condicion es true se ejecuta esta linea
+}
 
 /****** 2.4 Listas, Objetos y Fechas en JavaScript**********/
