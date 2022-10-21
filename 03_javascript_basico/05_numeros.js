f526776d014f7286a66a612edfe72a0e8e1216e3
Eugenia <costamariaeugenia1@gmail.com>
2022-10-21 17:21:45 -0300

    agrego al consigna del trabajo de entrega

--- 03_javascript_basico/05_numeros.js
+++ 03_javascript_basico/05_numeros.js
@@ -113,3 +113,15 @@ console.log(min_valor_JS); // 5e-324
 console.log(max_valor_JS); // 1.7976931348623157e+308
 
 /***** Ejercicio *****/
+// Crea un archivo JS que contenga las siguientes líneas
+// Una variable que contenga tu altura en centímetros (entero)
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+// Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
