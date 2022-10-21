9a2a65a87b64c27deb2d35afbafc5588ceade782
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-20 21:05:47 -0300

    finalizo el modulo dos de JavaScript

--- 03_javascript_basico/03_estructuras_de_control.js
+++ 03_javascript_basico/03_estructuras_de_control.js
@@ -1,7 +1,19 @@
 /******* 03 - ESTRUCTURAS DE CONTROL ******/
 
 /***** 3.1 Bifurcaciones: if, else, switch *****/
+// Bifurcaciones if...else
+let saldo = 50;
+let efectivo = 100;
 
+if (efectivo < saldo) {
+  console.log("Puedes sacar dinero")
+}
+
+if (efectivo < saldo) {
+  console.log("Puedes sacar dinero")
+} else {
+  console.log("Saldo insuficiente")
+}
 /***** 3.2 Comparaciones en JavaScript *****/
 
 /***** 3.3 Bucles for y While *****/
