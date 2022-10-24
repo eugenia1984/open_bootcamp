7d60751101f72cc59449d559c9dd670e17a16d3a
Eugenia <costamariaeugenia1@gmail.com>
2022-10-23 22:48:07 -0300

    agrego los temas

---
+++ 03_javascript_basico/09_gestion_errores.js
@@ -0,0 +1,7 @@
+/********* Gestion de errores *******/
+
+/*********** 9.1 Errores en JavaScript *********/
+
+/*********** 9.2 Gestion de logs en Nodejs *********/
+
+/*********** Ejercicio *********/
