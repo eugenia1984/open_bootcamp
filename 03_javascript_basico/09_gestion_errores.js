4c7502a0f57ccc75dc6135a33e441251f3535ad2
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-24 00:03:29 -0300

    hago el ejercicio de la practica 9

--- 03_javascript_basico/09_gestion_errores.js
+++ 03_javascript_basico/09_gestion_errores.js
@@ -58,3 +58,7 @@ console.error("Esto es un mensaje de error");
 console.debug("Esto es un mensjae de debug");
 
 /*********** Ejercicio *********/
+// Crea un nuevo proyecto de Node
+// Instala la dependencia Winston
+//  En el archivo index.js crea una función que devuelva un error con un mensaje personalizado
+// Registra el error en un archivo a través de un try...catch
--- 03_javascript_basico/sesion-09-gestion-errores/index.js
+++ 03_javascript_basico/sesion-09-gestion-errores/index.js
@@ -5,3 +5,13 @@ logger.info("Hola esto es un mensaje informativo")
 logger.debug("Esto es un mensaje de debug")
 logger.warn("Esto es un mensaje de advertencia")
 logger.error("Esto es un error")
+
+function showError() {
+  throw new Error("showError function");
+}
+
+try {
+  showError();
+} catch (e) {
+  logger.log("error", e.toString());
+}
