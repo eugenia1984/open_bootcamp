ff9d59306964d689f74fccc9fb4f6f1299521aed
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-23 22:44:36 -0300

    finalizo el ejercicio de la octava entrega

--- 03_javascript_basico/entregas/octava_entrega.js
+++ 03_javascript_basico/entregas/octava_entrega.js
@@ -10,5 +10,24 @@ const siempreVerdadera = verdadera();
 // muestro por consola
 console.log(siempreVerdadera);
 // - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
-
+async function damePromesa() {
+  return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
 //- Una función generadora de índices pares automáticos
+function* idsPares() {
+  let id = 0
+  while(true) {
+      yield id += 2
+  }
+}
+// invoco a la funcion y lo guardo en constante
+const gen = idsPares();
+// con el generetor utilizo el metodo .next y capturo su valor con value()
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
