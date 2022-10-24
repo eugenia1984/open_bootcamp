202bf6ad6578a1db7aa59752d2de9727a82b96e1
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-23 22:41:06 -0300

    copio consigna y comienzo la entrga octava

--- 03_javascript_basico/08_funciones.js
+++ 03_javascript_basico/08_funciones.js
@@ -178,3 +178,7 @@ console.log(gen.next().value)
 console.log(gen.next().value)
 
 /***** Ejercicio ********/
+// Crea un archivo JS que contenga las siguientes líneas
+// - Una función sin parámetros que devuelva siempre "true"
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+//- Una función generadora de índices pares automáticos
---
+++ 03_javascript_basico/entregas/octava_entrega.js
@@ -0,0 +1,14 @@
+/***** Ejercicio ********/
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una función sin parámetros que devuelva siempre "true"
+function verdadera() {
+  return true;
+}
+// invoco a mi funcion y guardo su resultado en la constante
+const siempreVerdadera = verdadera();
+// muestro por consola
+console.log(siempreVerdadera);
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+
+//- Una función generadora de índices pares automáticos
